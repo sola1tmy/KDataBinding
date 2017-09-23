889a2ed4cf3827e3c8c0724cba75eb7d65413b46
sola_tmy <sola1tmy@gmail.com>
2017-09-23 03:24:06 +0100

    add functions to bind 2 or 3 values

--- library/src/main/java/cn/sola/databinding/KDataBinding.kt
+++ library/src/main/java/cn/sola/databinding/KDataBinding.kt
@@ -9,7 +9,6 @@ import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 
-
 /**
  * Created by sola_tmy on 2017/9/8.
  * KDataBinding is a simple library to bind a property and a View
@@ -44,7 +43,7 @@ abstract class KDataBinding<in R, T> : ReadWriteProperty<R, T>{
 
 val TAG = "KDataBinding"
 val DEFAULT_HANDLER:Handler = Handler(Looper.getMainLooper())
-val DATA_BINDING_MAP = hashMapOf<String, KDataBinding<*, *>>()
+val DATA_BINDING_MAP = hashMapOf<KProperty<*>, KDataBinding<*, *>>()
 
 
 /**
@@ -67,14 +66,14 @@ class PropertyBinding<T>(private var realValue: T): KDataBinding<Any?, T>() {
  * When you want to get a value which not called canBeBind
  * An EmptyBinding will return
  */
-class EmptyBinding<R, T>:KDataBinding<R, T>() {
+class EmptyBinding<T>: KDataBinding<Any, T>() {
     override fun value(): T { throw Exception("This exception should never be thrown") }
 
     override fun bindListener(view: View, doWhat: () -> Unit){}
 
-    override fun getValue(thisRef: R, property: KProperty<*>): T { throw Exception("This exception should be never thrown") }
+    override fun getValue(thisRef: Any, property: KProperty<*>): T { throw Exception("This exception should be never thrown") }
 
-    override fun setValue(thisRef: R, property: KProperty<*>, value: T) {
+    override fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
         Log.e(TAG, "You've bind a view with a property which can not be bind")
     }
 
@@ -96,44 +95,43 @@ class DelegateBinding<in R, T>(private val parent: ReadWriteProperty<R, T>, priv
 
 }
 
-
-fun getBinding(key:String): KDataBinding<*, *>{
-    return try {
-        if (DATA_BINDING_MAP[key] == null) {
-            throw NullPointerException("")
-        } else {
-            DATA_BINDING_MAP[key]!!
-        }
-    } catch (e: Throwable){
-        e.printStackTrace()
-        EmptyBinding<Any, Any>()
-    }
-}
+internal fun <T> getBinding(property: KProperty<T>): KDataBinding<*, T> =
+        if (DATA_BINDING_MAP[property] == null)  EmptyBinding() else (DATA_BINDING_MAP[property] as KDataBinding<*, T>)
 
 fun TextView.bindText(property: KProperty<*>) {
-    val dataBinding: KDataBinding<*, *> = getBinding(property.name)
+    val dataBinding: KDataBinding<*, *> = getBinding(property)
     val todo = { this.text = dataBinding.value().toString() }
     dataBinding.bindListener(this, todo)
 }
 
-fun <T : View> T.bind(property: KProperty<*>, doWhat:T.(Any?)-> Unit) {
-    val dataBinding: KDataBinding<*, *> = getBinding(property.name)
+fun <T: View, R> T.bind(property: KProperty<R>, doWhat:T.(R?)-> Unit) {
+    val dataBinding: KDataBinding<*, R> = getBinding(property)
     val todo = { doWhat(dataBinding.value())}
     dataBinding.bindListener(this, todo)
 }
 
-fun <T : View> T.bind(key1:String, key2:String, doWhat:T.(Any?, Any?)-> Unit) {
-    val dataBinding1: KDataBinding<*, *> = getBinding(key1)
-    val dataBinding2: KDataBinding<*, *> = getBinding(key2)
+fun <T: View, R1, R2> T.bind(p1: KProperty<R1>, p2: KProperty<R2>, doWhat: (R1?, R2?) -> Unit) {
+    val dataBinding1: KDataBinding<*, R1> = getBinding(p1)
+    val dataBinding2: KDataBinding<*, R2> = getBinding(p2)
 
-    val todo = { doWhat(dataBinding1.value(), dataBinding2.value()) }
-
+    val todo = { doWhat(dataBinding1.value(), dataBinding2.value())}
     dataBinding1.bindListener(this, todo)
     dataBinding2.bindListener(this, todo)
 }
 
-fun <R, T> ReadWriteProperty<R, T>.canBeBind(thisRef: R, property: KProperty<*>): ReadWriteProperty<R, T> =
-        DelegateBinding(this, thisRef, property).apply { DATA_BINDING_MAP.put(property.name, this) }
+fun <T: View, R1, R2, R3> T.bind(p1: KProperty<R1>, p2: KProperty<R2>, p3: KProperty<R3>, doWhat: (R1?, R2?, R3?) -> Unit) {
+    val dataBinding1: KDataBinding<*, R1> = getBinding(p1)
+    val dataBinding2: KDataBinding<*, R2> = getBinding(p2)
+    val dataBinding3: KDataBinding<*, R3> = getBinding(p3)
 
-fun <T> canBeBind(property: KProperty<*>, value:T) =
-        PropertyBinding(value).apply { DATA_BINDING_MAP.put(property.name, this) }
+    val todo = { doWhat(dataBinding1.value(), dataBinding2.value(), dataBinding3.value())}
+    dataBinding1.bindListener(this, todo)
+    dataBinding2.bindListener(this, todo)
+    dataBinding3.bindListener(this, todo)
+}
+
+fun <R, T> ReadWriteProperty<R, T>.canBeBind(thisRef: R, property: KProperty<*>): ReadWriteProperty<R, T> =
+        DelegateBinding(this, thisRef, property).apply { DATA_BINDING_MAP.put(property, this) }
+
+fun <T> canBeBind(property: KProperty<*>, value: T) =
+        PropertyBinding(value).apply { DATA_BINDING_MAP.put(property, this) }
